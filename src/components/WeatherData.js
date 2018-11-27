d090699fdb3a614cdf9f46e5ec7a7336fa5fbc6d
jhonrsalcedo <jhonfor@gmail.com>
2018-11-27 17:01:40 -0500

    Create Component WeatherExtraInfo -  WeatherTemperature

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
 const WeatherData = () => (
-    <div>Weather Data</div>
+    <div>
+        <WeatherTemperature></WeatherTemperature>
+        <WeatherExtraInfo/>
+
+    </div>
+    
 );
 
 export default WeatherData;
