851424b81b55e93664dc922c9f8fee5cf49d7ad3
jhonrsalcedo <jhonfor@gmail.com>
2018-11-28 10:53:04 -0500

    Parametros aun Componente

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Location = () =>(
-    <div><h1>Cartagena - Colombia </h1></div>
-);
+/* se utiliza un parametro props, ademas se utiliza los {} para mas instrucciones 
+pero se utiliza de esa manera para asegurar que todo salga bien ya sea con una sola linea de codigo
+() */ 
+const Location = (props) =>{
+    console.log(props);
+/*se crea un punto de interrupcion debugger; */
+
+
+    return (
+    <div><h1>Cartagena - Colombia </h1></div>);
+};
 
 export default Location;
--- src/components/weatherLocation.js
+++ src/components/weatherLocation.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Location from  './Location';
 import WeatherData from './WeatherData';
 const WeatherLocation = () =>(
+    /* se le asigna un parametro a la etiqueta Location*/
     <div>
-        <Location></Location>
+        <Location city={"Cartagena de Indias"}></Location>
         <WeatherData></WeatherData>
     </div>
 );
